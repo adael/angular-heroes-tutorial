25a8b781ac200f3905344e2b658048748a135bdf
Carlos Gant <adaelxp@gmail.com>
2017-04-08 03:23:28 +0200

    tutorial

---
+++ README.md
@@ -0,0 +1,10 @@
+# TOUR OF HEROES TUTORIAL FOR ANGULAR 4
+
+Here's my take on this nice and fun tutorial.
+https://angular.io/docs/ts/latest/tutorial/
+
+# How to run
+
+* clone
+* npm install
+* npm start
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
+
+})
+export class AppModule {
+}
